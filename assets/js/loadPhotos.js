b18e471a9664c10659fc6a8e9102bf3516e17755
Pavel Merkushevich <11mpa@mail.ru>
2022-05-08 13:30:37 +0300

    Add intro load animation

---
+++ assets/js/loadPhotos.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function () {
+    $('.intro').css({
+        'display': 'flex'
+    });
+    $('.intro-load-animation-container').css({
+        'display': 'none'
+    });
+});
